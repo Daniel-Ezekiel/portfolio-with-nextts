1ba125dfc61a1edb139d26773d4833e389868cce
Daniel Ezekiel <dbezekiel77@gmail.com>
2024-09-26 21:39:54 +0100

    chore: added google verification html file

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import { motion } from "framer-motion";
 
 const Button = ({
   text,
@@ -17,18 +18,20 @@ const Button = ({
   className?: string;
 }) => {
   return (
-    <Link
-      href={href}
-      target={target}
-      aria-label={ariaLabel}
-      className={`px-4 py-2 rounded-2xl ${
-        !hasHover
-          ? "bg-gradient-linear-300 border-1 border-transparent font-bold uppercase"
-          : "border-gray-200 border-1 hover:border-0 hover:bg-gradient-linear-300"
-      } hover:scale-110 hover:shadow-lg hover:shadow-black/30 transition-all ease-in-out duration-300 xl:py-3 ${className}`}
-    >
-      {text}
-    </Link>
+    <div>
+      <Link
+        href={href}
+        target={target}
+        aria-label={ariaLabel}
+        className={`px-4 py-2 rounded-2xl ${
+          !hasHover
+            ? "bg-gradient-linear-300 border-1 border-transparent font-bold uppercase"
+            : "border-gray-200 border-1 hover:border-0 hover:bg-gradient-linear-300"
+        } hover:scale-110 hover:shadow-lg hover:shadow-black/30 transition-all ease-in-out duration-300 xl:py-3 ${className}`}
+      >
+        {text}
+      </Link>
+    </div>
   );
 };
 
